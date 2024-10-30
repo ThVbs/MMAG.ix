d143808f926636e11061061234d9d6f0d20066c0
Maustex <mauricio_teixeira1@estudante.sesisenai.org.br>
2024-10-30 17:03:28 -0300

    Update login.js

--- musicas2/login/login.js
+++ musicas2/login/login.js
@@ -10,7 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     loginBTN.addEventListener("click", () => {
-        window.location.href = "../paginicial/inicio.html";
+        
     });
 });
-
