b0de236d02b9a836f19572628403e8513d85428d
Maustex <mauricio_teixeira1@estudante.sesisenai.org.br>
2024-11-02 16:00:57 -0300

    Add files via upload

---
+++ musicas2/musicatocando2/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const AnteriorBTN = document.querySelector(".antes");
+    const container = document.querySelector(".container");
+    
+    AnteriorBTN.addEventListener("click", () => {
+        window.location.href = "../musicatocando/index.html";
+    });
+
+});
+document.addEventListener("DOMContentLoaded", () => {
+    const playBTN = document.querySelector(".play");
+    const backgroundMusic = document.getElementById("backgroundMusic");
+
+    playBTN.addEventListener("click", () => {
+        if (backgroundMusic.paused) {
+            backgroundMusic.play(); 
+            playBTN.textContent = "❚❚";
+        } else {
+            backgroundMusic.pause(); 
+            playBTN.textContent = "►";
+        }
+    });
+});
