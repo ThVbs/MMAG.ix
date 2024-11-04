db7f54961fe5b0756dce4e3e660bea81fb5a10e1
Maustex <mauricio_teixeira1@estudante.sesisenai.org.br>
2024-11-04 20:58:34 -0300

    Update script.js

--- musicas2/musicatocando2/script.js
+++ musicas2/musicatocando2/script.js
@@ -1,12 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
     const AnteriorBTN = document.querySelector(".antes");
-    const container = document.querySelector(".container");
+    const inicioBTN = document.querySelector(".inicio");
+    const usuarioBTN = document.querySelector(".usuario")
+    
+    inicioBTN.addEventListener("click", () => {
+        window.location.href = "../telainicio/index.html";
+    });
+
+    usuarioBTN.addEventListener("click", () => {
+        window.location.href = "../paginicial/inicio.html";
+    });
     
     AnteriorBTN.addEventListener("click", () => {
         window.location.href = "../musicatocando/index.html";
     });
 
 });
+
 document.addEventListener("DOMContentLoaded", () => {
     const playBTN = document.querySelector(".play");
     const backgroundMusic = document.getElementById("backgroundMusic");
