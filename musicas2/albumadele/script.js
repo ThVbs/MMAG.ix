7ab92f115705a30dcdb6ca70cc0ecaf5161b5198
Maustex <mauricio_teixeira1@estudante.sesisenai.org.br>
2024-11-04 15:08:27 -0300

    Add files via upload

---
+++ musicas2/albumadele/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const musicaBtn = document.querySelector(".musica");
+    
+    musicaBtn.addEventListener("click", () => {
+        window.location.href = "../musicatocando/index.html";
+    });
+    
+
+});
