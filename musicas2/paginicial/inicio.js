8249bcf2a0cd47d21523bcf7ad13526dafb919b2
Maustex <mauricio_teixeira1@estudante.sesisenai.org.br>
2024-11-06 14:26:27 -0300

    Update inicio.js

--- musicas2/paginicial/inicio.js
+++ musicas2/paginicial/inicio.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const inicioBTN = document.querySelector(".inicio");
+    const inicioBTN = document.querySelector(".logo");
     
     inicioBTN.addEventListener("click", () => {
         window.location.href = "../telainicio/index.html";
