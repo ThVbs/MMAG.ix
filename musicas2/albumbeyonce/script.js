cd49997d27c20d261bae0da8886d9c0ddbe9ee10
Anna <anna_cm_santos@estudante.sesisenai.org.br>
2024-11-13 14:13:35 -0300

    Add files via upload

---
+++ musicas2/albumbeyonce/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const musicaBtn = document.querySelector(".musica");
+    
+    musicaBtn.addEventListener("click", () => {
+        window.location.href = "../musicatocando/index.html";
+    });
+    
+    
+
+});
