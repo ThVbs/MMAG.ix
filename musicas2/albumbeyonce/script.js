3ed29c0703f2d27979bb72ef4a96b479bfaca7a6
Maustex <mauricio_teixeira1@estudante.sesisenai.org.br>
2024-11-18 15:52:39 -0300

    Update script.js

--- musicas2/albumbeyonce/script.js
+++ musicas2/albumbeyonce/script.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     const musicaBtn = document.querySelector(".musica");
+    const inicioBTN = document.querySelector(".inicio")
     
     musicaBtn.addEventListener("click", () => {
         window.location.href = "../musicatocando/index.html";
     });
-    
+
+    inicioBTN.addEventListener("click", () => {
+        window.location.href = "../telainicio/index.html";
+    });
     
 
 });
