2186240eaa24ae1a6e33437e5ad8173ecd3fc804
Maustex <mauricio_teixeira1@estudante.sesisenai.org.br>
2024-11-18 15:53:40 -0300

    Update script.js

--- musicas2/albumroberto/script.js
+++ musicas2/albumroberto/script.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     const musicaBtn = document.querySelector(".musica");
+    const inicioBTN = document.querySelector(".inicio")
     
     musicaBtn.addEventListener("click", () => {
         window.location.href = "../musicatocando/index.html";
+    });
+
+    inicioBTN.addEventListener("click", () => {
+        window.location.href = "../telainicio/index.html";
     });
     
 
