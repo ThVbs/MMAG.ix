1646f67fe30837c1f1be2fa55a7862a6908d154c
Maustex <mauricio_teixeira1@estudante.sesisenai.org.br>
2024-11-18 15:50:49 -0300

    Update script.js

--- musicas2/albumadele/script.js
+++ musicas2/albumadele/script.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     const musicaBtn = document.querySelector(".musica");
+    const inicioBTN = document.querySelector(".inicio");
+    const musicaBTN = document.querySelector(".Skyfall")
     
     musicaBtn.addEventListener("click", () => {
         window.location.href = "../musicatocando/index.html";
     });
-    
 
+    inicioBTN.addEventListener("click", () => {
+        window.location.href = "../telainicio/index.html";
+    });
+    
+    musicaBTN.addEventListener("click", () => {
+        window.location.href = "../musicatocando4/index.html";
+    });
 });
+V
