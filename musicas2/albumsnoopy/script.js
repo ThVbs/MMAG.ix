64c34045ede190c78d78dd4e6515e05a519ccfe8
Maustex <mauricio_teixeira1@estudante.sesisenai.org.br>
2024-11-25 16:22:43 -0300

    Update script.js

--- musicas2/albumsnoopy/script.js
+++ musicas2/albumsnoopy/script.js
@@ -1,28 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const musicaBtn = document.querySelector(".musica");
-    const inicioBTN = document.querySelector(".inicio")
-    
-    musicaBtn.addEventListener("click", () => {
-        window.location.href = "../musicatocando/index.html";
-    });
-
-    inicioBTN.addEventListener("click", () => {
-        window.location.href = "../telainicio/index.html";
-    });
-    
-
-});
-document.addEventListener("DOMContentLoaded", () => {
-    const playBTN = document.querySelector(".play");
-    const backgroundMusic = document.getElementById("backgroundMusic");
-
-    playBTN.addEventListener("click", () => {
-        if (backgroundMusic.paused) {
-            backgroundMusic.play(); 
-            playBTN.textContent = "❚❚";
-        } else {
-            backgroundMusic.pause();
-            playBTN.textContent = "►";
-        }
-    })
-})
+<div id="txt">
+        
+    </div>
+      
+        <div id="innerHTMLtxt"></div>
+      <div id="textContenttxt"><div>
