5d3b45e9ae89beb873bbd442652ffa301740722c
Maria Luiza Camargo <maria_l_camargo@estudante.sesisenai.org.br>
2024-11-27 15:14:18 -0300

    Update script.js

--- musicas2/albumbeyonce/script.js
+++ musicas2/albumbeyonce/script.js
@@ -2,8 +2,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const musicaBtn = document.querySelector(".musica");
     const inicioBTN = document.querySelector(".inicio");
+    const cil = document.querySelector(".cil");
+    const sl = document.querySelector(".sl");
+    const texas = document.querySelector(".texas");
+    const dy = document.querySelector(".dy");
 
-     document.getElementById("txt").style.display = "none"; //oculta a barra
+    document.getElementById("txt").style.display = "none"; //oculta a barra
     
     musicaBtn.addEventListener("click", () => {
         window.location.href = "../musicatocando/index.html";
@@ -13,6 +17,21 @@ document.addEventListener("DOMContentLoaded", () => {
         window.location.href = "../telainicio/index.html";
     });
     
+    cil.addEventListener("click", () => {
+        window.location.href = "../musicatocando11/index.html";
+    });
+
+    sl.addEventListener("click", () => {
+        window.location.href = "../musicatocando12/index.html";
+    });
+
+    texas.addEventListener("click", () => {
+        window.location.href = "../musicatocando13/index.html";
+    });
+
+    dy.addEventListener("click", () => {
+        window.location.href = "../musicatocando14/index.html";
+    });
 
 });
 document.addEventListener("DOMContentLoaded", () => {
