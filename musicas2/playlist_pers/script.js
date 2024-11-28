1e5e89d22e78cd9440ff7339efbd4d51033a7e13
GECIELSCHW <geciel_schwinden@estudante.sesisenai.org.br>
2024-11-28 17:18:06 -0300

    Update script.js

--- musicas2/playlist_pers/script.js
+++ musicas2/playlist_pers/script.js
@@ -1,12 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const playlistItems = document.querySelectorAll(".playlist-item");
+
+    playlistItems.forEach(item => {
+        item.addEventListener("click", () => {
+            const link = item.getAttribute("data-link");
+            if (link) {
+                window.location.href = link;
+            }
+        });
+    });
+
+    // Botão para logo
     const inicioBTN = document.querySelector(".logo");
-    
     inicioBTN.addEventListener("click", () => {
         window.location.href = "../telainicio/index.html";
     });
 
-});
-const usuarioBTN = document.querySelector(".Usuario");
-usuarioBTN.addEventListener("click", () => {
-    window.location.href = "../paginicial/inicio.html";
+    // Botão para usuário
+    const usuarioBTN = document.querySelector(".Usuario");
+    usuarioBTN.addEventListener("click", () => {
+        window.location.href = "../paginicial/inicio.html";
+    });
 });
