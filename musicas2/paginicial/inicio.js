f2ab663a98c277951f6e04d6e552f391386cf0f5
Maustex <mauricio_teixeira1@estudante.sesisenai.org.br>
2024-11-27 21:19:57 -0300

    Update inicio.js

--- musicas2/paginicial/inicio.js
+++ musicas2/paginicial/inicio.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     const inicioBTN = document.querySelector(".logo");
+    const playlist = document.querySelector(".playlist");
+    const album = document.querySelector(".albuns");
     
+    album.addEventListener("click", () => {
+        window.location.href = "../albuns/index.html";
+    });
+
+    playlist.addEventListener("click", () => {
+        window.location.href = "../playlist_pers/index.html";
+    });
+
     inicioBTN.addEventListener("click", () => {
         window.location.href = "../telainicio/index.html";
     });
