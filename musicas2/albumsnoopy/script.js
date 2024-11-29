bf33af16c062412ea7adea790eba7b84e05420c0
Maustex <mauricio_teixeira1@estudante.sesisenai.org.br>
2024-11-29 00:25:28 -0300

    Update script.js

--- musicas2/albumsnoopy/script.js
+++ musicas2/albumsnoopy/script.js
@@ -1,6 +1,55 @@
-<div id="txt">
-        
-    </div>
-      
-        <div id="innerHTMLtxt"></div>
-      <div id="textContenttxt"><div>
+document.addEventListener("DOMContentLoaded", () => {
+  const musicaBtn = document.querySelector(".musica");
+  const inicioBTN = document.querySelector(".inicio");
+  const yw = document.querySelector(".yw");
+  const di = document.querySelector(".di");
+  const ds = document.querySelector(".ds");
+  const workout = document.querySelector(".workout");
+
+  document.getElementById("txt").style.display = "none"; //oculta a barra
+  
+  musicaBtn.addEventListener("click", () => {
+      window.location.href = "../musicatocando/index.html";
+  });
+
+  inicioBTN.addEventListener("click", () => {
+      window.location.href = "../telainicio/index.html";
+  });
+  
+  yw.addEventListener("click", () => {
+      window.location.href = "../musicatocando23/index.html";
+  });
+
+  di.addEventListener("click", () => {
+      window.location.href = "../musicatocando24/index.html";
+  });
+
+  ds.addEventListener("click", () => {
+      window.location.href = "../musicatocando25/index.html";
+  });
+
+  workout.addEventListener("click", () => {
+      window.location.href = "../musicatocando26/index.html";
+  });
+
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const playBTN = document.querySelector(".play");
+  const backgroundMusic = document.getElementById("backgroundMusic");
+
+  playBTN.addEventListener("click", () => {
+      if (backgroundMusic.paused) {
+          backgroundMusic.play(); 
+          playBTN.textContent = "❚❚";
+      } else {
+          backgroundMusic.pause();
+          playBTN.textContent = "►";
+      }
+  })
+})
+
+function menu(){
+  document.getElementById("txt").style.display = "block"; //mostra a barra
+  
+}
+
