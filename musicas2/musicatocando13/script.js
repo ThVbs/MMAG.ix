016a35785cc0b8604222de673ac7d7f3cd3f349d
Maria Luiza Camargo <maria_l_camargo@estudante.sesisenai.org.br>
2024-12-03 17:32:30 -0300

    Update script.js

--- musicas2/musicatocando13/script.js
+++ musicas2/musicatocando13/script.js
@@ -20,30 +20,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
    
     const lyrics = [
-        { time: 32, element: document.getElementById('line1') },
-        { time: 37, element: document.getElementById('line2') },
-        { time: 44, element: document.getElementById('line3') },
-        { time: 50, element: document.getElementById('line4') },
-        { time: 58, element: document.getElementById('line5') },
-        { time: 63, element: document.getElementById('line6') },
-        { time: 69, element: document.getElementById('line7') },
-        { time: 75, element: document.getElementById('line8') },
-        { time: 82, element: document.getElementById('line9') },
-        { time: 86, element: document.getElementById('line10') },
-        { time: 89, element: document.getElementById('line11') },
-        { time: 92, element: document.getElementById('line12') },
-        { time: 96, element: document.getElementById('line13') },
-        { time: 100, element: document.getElementById('line14') },
-        { time: 102, element: document.getElementById('line15') },
-        { time: 105, element: document.getElementById('line16') },
-        { time: 108, element: document.getElementById('line17') },
-        { time: 113, element: document.getElementById('line18') },
+        { time: 12, element: document.getElementById('line1') },
+        { time: 16, element: document.getElementById('line2') },
+        { time: 20, element: document.getElementById('line3') },
+        { time: 25, element: document.getElementById('line4') },
+        { time: 30, element: document.getElementById('line5') },
+        { time: 32, element: document.getElementById('line6') },
+        { time: 34, element: document.getElementById('line7') },
+        { time: 37, element: document.getElementById('line8') },
+        { time: 40, element: document.getElementById('line9') },
+        { time: 45, element: document.getElementById('line10') },
+        { time: 49, element: document.getElementById('line11') },
+        { time: 53, element: document.getElementById('line12') },
+        { time: 102, element: document.getElementById('line13') },
+        { time: 107, element: document.getElementById('line14') },
+        { time: 111, element: document.getElementById('line15') },
+        
+        
     ];
 
     
     function updateLyrics(currentTime) {
         lyrics.forEach(({ time, element }) => {
-            if (currentTime >= time && currentTime < time + 4) {
+            if (currentTime >= time && currentTime < time + 3) {
                 element.classList.add('active');
             } else {
                 element.classList.remove('active');
