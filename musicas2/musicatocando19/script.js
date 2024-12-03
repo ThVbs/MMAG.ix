1b8569a2a0306d1fc10ccb6f2c9f2ed4f520d2de
Maustex <mauricio_teixeira1@estudante.sesisenai.org.br>
2024-12-03 17:22:26 -0300

    Update script.js

--- musicas2/musicatocando19/script.js
+++ musicas2/musicatocando19/script.js
@@ -20,13 +20,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
    
     const lyrics = [
-        { time: 32, element: document.getElementById('line1') },
-        { time: 37, element: document.getElementById('line2') },
-        { time: 44, element: document.getElementById('line3') },
-        { time: 50, element: document.getElementById('line4') },
-        { time: 58, element: document.getElementById('line5') },
-        { time: 63, element: document.getElementById('line6') },
-        { time: 69, element: document.getElementById('line7') },
+        { time: 15, element: document.getElementById('line1') },
+        { time: 22, element: document.getElementById('line2') },
+        { time: 30, element: document.getElementById('line3') },
+        { time: 38, element: document.getElementById('line4') },
+        { time: 44, element: document.getElementById('line5') },
+        { time: 50, element: document.getElementById('line6') },
+        { time: 57, element: document.getElementById('line7') },
         { time: 75, element: document.getElementById('line8') },
         { time: 82, element: document.getElementById('line9') },
         { time: 86, element: document.getElementById('line10') },
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
     
     function updateLyrics(currentTime) {
         lyrics.forEach(({ time, element }) => {
-            if (currentTime >= time && currentTime < time + 4) {
+            if (currentTime >= time && currentTime < time + 5) {
                 element.classList.add('active');
             } else {
                 element.classList.remove('active');
