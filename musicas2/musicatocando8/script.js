8cc71f029b0f816b800f87eb6de0542b2618f13c
Anna <anna_cm_santos@estudante.sesisenai.org.br>
2024-12-03 17:18:23 -0300

    Update script.js

--- musicas2/musicatocando8/script.js
+++ musicas2/musicatocando8/script.js
@@ -20,24 +20,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
    
     const lyrics = [
-        { time: 32, element: document.getElementById('line1') },
-        { time: 37, element: document.getElementById('line2') },
-        { time: 44, element: document.getElementById('line3') },
-        { time: 50, element: document.getElementById('line4') },
-        { time: 58, element: document.getElementById('line5') },
-        { time: 63, element: document.getElementById('line6') },
-        { time: 69, element: document.getElementById('line7') },
-        { time: 75, element: document.getElementById('line8') },
-        { time: 82, element: document.getElementById('line9') },
-        { time: 86, element: document.getElementById('line10') },
-        { time: 89, element: document.getElementById('line11') },
-        { time: 92, element: document.getElementById('line12') },
-        { time: 96, element: document.getElementById('line13') },
-        { time: 100, element: document.getElementById('line14') },
-        { time: 102, element: document.getElementById('line15') },
-        { time: 105, element: document.getElementById('line16') },
-        { time: 108, element: document.getElementById('line17') },
-        { time: 113, element: document.getElementById('line18') },
+        { time: 80, element: document.getElementById('line1') },
+        { time: 86, element: document.getElementById('line2') },
+        { time: 92, element: document.getElementById('line3') },
+        { time: 98, element: document.getElementById('line4') },
+        { time: 105, element: document.getElementById('line5') },
+        { time: 110, element: document.getElementById('line6') },
+        { time: 116, element: document.getElementById('line7') },
+        { time: 122, element: document.getElementById('line8') },
+        { time: 128, element: document.getElementById('line9') },
+        { time: 134, element: document.getElementById('line10') },
+        { time: 142, element: document.getElementById('line11') },
     ];
 
     
